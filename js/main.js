e1ca44bed8018ed9b0a663068618717eeb3f75d0
“Katanpe” <katja.peltonen3@metropolia.fi>
2022-03-07 10:44:37 +0200

    Reititys toimii, mutta kartassa näkyy vielä kaikki haetut reitit.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,77 @@
 'use strict';
 
 //lisätään kartta sivustolle
-const map = L.map('map');
+const map = L.map('map').setView([60.172659, 24.926596],11);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-//API, jota käytetään reitin hakuun
-const routingAPI = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
+//paikkatiedot haettu onnistuneesti
+function success(pos) {
+  const crd = pos.coords;
+  console.log(crd);
+
+  //lisätään oman sijainnin marker
+  const ownLocation = addMarker(crd, 'Olen tässä!');
+  ownLocation.openPopup();
+
+  //lisätään markerit kohdepaikoille
+  getLocations(crd).then(function(pointsOfInterest) {
+    for (let i = 0; i < pointsOfInterest.length; i++) {
+      const placeName = pointsOfInterest[i].name_fi;
+      const coordinates = {
+        latitude: pointsOfInterest[i].latitude,
+        longitude: pointsOfInterest[i].longitude,
+      };
+      const marker = addMarker(coordinates, placeName);
+      //haetaan tiedot yhdestä pisteestä ja reitti sinne
+      marker.on('click', function(){
+        document.querySelector('#name').innerHTML = pointsOfInterest[i].name_fi;
+        document.querySelector('#address').innerHTML = pointsOfInterest[i].street_address_fi;
+        document.querySelector('#city').innerHTML = pointsOfInterest[i].address_city_fi;
+        const targetCrd = {
+          latitude: pointsOfInterest[i].latitude,
+          longitude: pointsOfInterest[i].longitude,
+        };
+        console.log(targetCrd);
+        document.querySelector('#buttonNavigation').addEventListener('click', function () {
+          console.log('Klikattu!');
+          getRoute(crd, targetCrd);
+        })
+      })
+    }
+  });
+}
+
+//paikkatietojen hakuasetukset
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+//paikkatietojen hakemisessa on tapahtunut virhe
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+//omien paikkatietojen haku
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+//kohdesijaintien haku Helsingin kaupungin APISTA
+function getLocations(crd) {
+  const url = 'https://www.hel.fi/palvelukarttaws/rest/v4/unit/?ontologyword=317+318+319+320+321+322+323';
+  return fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(pointsOfInterest) {
+    console.log(pointsOfInterest);
+    return pointsOfInterest;
+  });
+}
 
 // haetaan reitti lähtöpisteen ja kohteen avulla
 function getRoute(lahto, kohde) {
+  const routingAPI = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
   // GraphQL haku
   const haku = `{
   plan(
@@ -45,7 +106,7 @@ function getRoute(lahto, kohde) {
   fetch(routingAPI, fetchOptions).then(function (vastaus) {
     return vastaus.json();
   }).then(function (tulos) {
-    //console.log(tulos.data.plan.itineraries[0].legs);
+    console.log(tulos.data.plan.itineraries[0].legs);
     const googleKoodattuReitti = tulos.data.plan.itineraries[0].legs;
     for (let i = 0; i < googleKoodattuReitti.length; i++) {
       let color = '';
@@ -75,64 +136,6 @@ function getRoute(lahto, kohde) {
     map.fitBounds([[lahto.latitude, lahto.longitude], [kohde.latitude, kohde.longitude]]);
   }).catch(function (e) {
     console.error(e.message);
-  });
-}
-
-//paikkatietojen hakuasetukset
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-
-//paikkatiedot haettu onnistuneesti
-function success(pos) {
-  const crd = pos.coords;
-  console.log(crd);
-
-  //lisätään oman sijainnin marker
-  const ownLocation = addMarker(crd, 'Olen tässä!');
-  ownLocation.openPopup();
-
-  //lisätään markerit kohdepaikoille
-  getLocations(crd).then(function(pointsOfInterest) {
-    for (let i = 0; i < pointsOfInterest.length; i++) {
-      const placeName = pointsOfInterest[i].name_fi;
-      const coordinates = {
-        latitude: pointsOfInterest[i].latitude,
-        longitude: pointsOfInterest[i].longitude,
-      };
-      const marker = addMarker(coordinates, placeName);
-      marker.on('click', function(){
-        document.querySelector('#name').innerHTML = pointsOfInterest[i].name_fi;
-        document.querySelector('#address').innerHTML = pointsOfInterest[i].street_address_fi;
-        document.querySelector('#city').innerHTML = pointsOfInterest[i].address_city_fi;
-        console.log(pointsOfInterest[i]);
-      })
-    }
-  });
-
-  navigationListener();
-
-  getRoute(crd, {latitude: 60.1636578, longitude: 24.9289657})
-}
-
-//paikkatietojen hakemisessa on tapahtunut virhe
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-//omien paikkatietojen haku
-navigator.geolocation.getCurrentPosition(success, error, options);
-
-//kohdesijaintien haku Helsingin kaupungin APISTA
-function getLocations(crd) {
-  const url = 'https://www.hel.fi/palvelukarttaws/rest/v4/unit/?ontologyword=317+318+319+320+321+322+323';
-  return fetch(url).then(function(response) {
-    return response.json();
-  }).then(function(pointsOfInterest) {
-    console.log(pointsOfInterest);
-    return pointsOfInterest;
   });
 }
 
